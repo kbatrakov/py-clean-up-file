85bd9a3e2e4373ff055db5844ee2e7144b176694
kbatrakov <kirillbatrakov0819@gmail.com>
2025-05-04 17:32:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
+from __future__ import annotations
 import os
-from typing import TextIO
 from typing import Optional, Type
 from types import TracebackType
 
@@ -7,14 +7,11 @@ from types import TracebackType
 class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
-        self.file = None
 
-    def __enter__(self) -> TextIO:
-        self.file = open(self.filename)
-        return self.file
+    def __enter__(self) -> CleanUpFile:
+        return self
 
     def __exit__(self, exc_type: Optional[Type[BaseException]] ,
                  exc_val: Optional[BaseException], exc_tb:
                  Optional[TracebackType]) -> None:
-        self.file.close()
         os.remove(self.filename)
